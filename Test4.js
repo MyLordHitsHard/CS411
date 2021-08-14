bd0c8974f4a75ca114fba9cf9c95ee2884bdc18f
Himanshu Dutt <dr.himanshudutt@gmail.com>
2021-08-14 17:55:47 +0530

    CS311 sem details

---
+++ Test4.js
@@ -0,0 +1,19 @@
+$(".btn3").click(function() {
+
+
+    $(".btn1").removeClass("hiddenclass");
+    $(".btn2").removeClass("hiddenclass");
+    $(".btn3").addClass("hiddenclass");
+});
+
+$(".btn1").click (function () {
+
+    $("table").removeClass("hiddenclass");
+    $(".subjectgrid").addClass("hiddenclass");
+});
+
+$(".btn2").click (function () {
+
+    $("table").addClass("hiddenclass");
+    $(".subjectgrid").removeClass("hiddenclass");
+});
